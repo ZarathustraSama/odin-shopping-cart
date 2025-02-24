27662659b674e8e92735dc74f89a5fd82afc204a
ZarathustraSama <mr.gabriel@live.it>
2025-02-24 17:13:30 +0100

    Added simple fetch to fakestoreapi, with simple styling

--- src/components/ShoppingPage/ShoppingPage.jsx
+++ src/components/ShoppingPage/ShoppingPage.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
 import NavigationBar from "../NavigationBar/NavigationBar";
+import ProductCard from "../ProductCard/ProductCard";
+
+import './ShoppingPage.module.css';
 
 export default function ShoppingPage() {
-  <>
-    <NavigationBar></NavigationBar>
-  </>
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products?limit=10')
+      .then(response => response.json())
+      .then(json => setProducts(json))
+      .catch(error => console.log(error))
+    
+  }, [])
+
+  return (
+    <>
+      <NavigationBar></NavigationBar>
+      <main>
+        {products.map(product => {
+          return <ProductCard key={product.id} product={product}></ProductCard>
+        })}
+      </main>
+    </>
+  )
 }
