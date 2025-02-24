93f0459920584ff66bf2eb8256dfd610dc1605eb
ZarathustraSama <mr.gabriel@live.it>
2025-02-24 17:17:50 +0100

    Added basic product info with simple styling and proptypes

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,10 +2,25 @@ import QuantityInput from "../QuantityInput/QuantityInput";
 import IncrementBtn from "../IncrementBtn/IncrementBtn";
 import DecrementBtn from "../DecrementBtn/DecrementBtn";
 
-export default function ProductCard() {
-  <>
-    <QuantityInput></QuantityInput>
-    <IncrementBtn></IncrementBtn>
-    <DecrementBtn></DecrementBtn>
-  </>
+import PropTypes from "prop-types";
+
+import './ProductCard.module.css';
+
+function ProductCard({ product }) {
+
+  return (
+    <section>
+      <h2>{product.title}</h2>
+      <img src={product.image} />
+      <QuantityInput></QuantityInput>
+      <IncrementBtn></IncrementBtn>
+      <DecrementBtn></DecrementBtn>
+    </section>
+  )
 }
+
+ProductCard.propTypes = {
+  product: PropTypes.object.isRequired,
+}
+
+export default ProductCard;
