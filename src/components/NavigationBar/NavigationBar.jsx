1c64fa8c299e9526a4091c619bbd5ada68159819
ZarathustraSama <mr.gabriel@live.it>
2025-02-24 17:12:15 +0100

    Added links, with simple styling

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import ShoppingCart from "../ShoppingCart/ShoppingCart";
 import CheckoutBtn from "../CheckoutBtn/CheckoutBtn";
 
+import './NavigationBar.module.css';
+
 export default function NavigationBar() {
-  <>
-    <CheckoutBtn></CheckoutBtn>
-  </>
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/shop">Shop</Link>
+      <ShoppingCart></ShoppingCart>
+      <CheckoutBtn></CheckoutBtn>
+    </nav>
+  )
 }
