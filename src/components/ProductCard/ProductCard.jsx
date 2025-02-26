e94d2ce9cb6cc93a50bb5090f9a17e1f5ce094d4
ZarathustraSama <mr.gabriel@live.it>
2025-02-26 17:16:17 +0100

    removed atomic components, added quantity state, prop for adding element to cart, added prop validation

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,26 +1,50 @@
-import QuantityInput from "../QuantityInput/QuantityInput";
-import IncrementBtn from "../IncrementBtn/IncrementBtn";
-import DecrementBtn from "../DecrementBtn/DecrementBtn";
-
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 import './ProductCard.module.css';
 
-function ProductCard({ product }) {
+
+function ProductCard({ product, addToCartFn }) {
+  const [quantity, setQuantity] = useState(0);
+
+  function onChangeInput(e) {
+    setQuantity(e.target.value);
+  }
+
+  function incrementQuantity() {
+    const newValue = quantity + 1;
+    setQuantity(newValue);
+  }
+
+  function decrementQuantity() {
+    if (quantity <= 0) { return };
+    
+    const newValue = quantity - 1;
+    setQuantity(newValue);
+  }
+
+  function addToCart() {
+    const newProduct = {...product, quantity: quantity};
+
+    addToCartFn(newProduct);
+  }
 
   return (
     <section>
       <h2>{product.title}</h2>
       <img src={product.image} />
-      <QuantityInput></QuantityInput>
-      <IncrementBtn></IncrementBtn>
-      <DecrementBtn></DecrementBtn>
+      <p>The price is {product.price}</p>
+      <input className="quantityInput" onChange={onChangeInput} type="number"></input>
+      <button className="incrementQuantityBtn" onClick={incrementQuantity}>+</button>
+      <button className="decrementQuantityBtn" onClick={decrementQuantity}>-</button>
+      <button className="addToCartBtn" onClick={addToCart}>Add To Cart</button>
     </section>
   )
 }
 
 ProductCard.propTypes = {
   product: PropTypes.object.isRequired,
+  addToCartFn: PropTypes.func.isRequired,
 }
 
 export default ProductCard;
