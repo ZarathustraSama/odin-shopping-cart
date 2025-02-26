78cfe59279450736e7e9e7a2143aead5d4418739
ZarathustraSama <mr.gabriel@live.it>
2025-02-26 17:14:09 +0100

    Added cart prop with validation

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,13 +1,21 @@
+import PropTypes from "prop-types";
+
 import NavigationBar from "../NavigationBar/NavigationBar";
 
 import './HomePage.module.css';
 
-export default function HomePage() {
+function HomePage({ shoppingCart = [] }) {
   return(
     <>
-      <NavigationBar></NavigationBar>
+      <NavigationBar shoppingCart={shoppingCart}></NavigationBar>
       <h1>Welcome to this incredible service that doesn&apos;t expire!</h1>
       <p>This is just a fac-simile for an e-commerce site. Feel Free to navigate the pages via the links above and take a look!</p>
     </>
   )
 }
+
+HomePage.propTypes = {
+  shoppingCart: PropTypes.array.isRequired,
+}
+
+export default HomePage;
