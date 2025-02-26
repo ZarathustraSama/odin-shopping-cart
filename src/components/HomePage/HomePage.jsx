0a18aa35284c5c57bd07364636c1603519b8e496
ZarathustraSama <mr.gabriel@live.it>
2025-02-26 16:21:38 +0100

    Added flavor to page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,7 +6,8 @@ export default function HomePage() {
   return(
     <>
       <NavigationBar></NavigationBar>
-      <p>Welcome to this incredible service that doesn&apos;t expire!</p>
+      <h1>Welcome to this incredible service that doesn&apos;t expire!</h1>
+      <p>This is just a fac-simile for an e-commerce site. Feel Free to navigate the pages via the links above and take a look!</p>
     </>
   )
 }
