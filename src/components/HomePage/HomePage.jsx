e0e939a87c95f1e087a58ade27e6362495bff83c
ZarathustraSama <mr.gabriel@live.it>
2025-02-26 18:49:33 +0100

    Refactored line

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ import './HomePage.module.css';
 function HomePage({ shoppingCart = [] }) {
   return(
     <>
-      <NavigationBar shoppingCart={shoppingCart}></NavigationBar>
+      <NavigationBar shoppingCart={shoppingCart} />
       <h1>Welcome to this incredible service that doesn&apos;t expire!</h1>
       <p>This is just a fac-simile for an e-commerce site. Feel Free to navigate the pages via the links above and take a look!</p>
     </>
