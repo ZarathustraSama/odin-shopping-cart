a08e2e0fa77cce90f25d4b31994be8f0daa2b6c4
ZarathustraSama <mr.gabriel@live.it>
2025-02-26 17:33:30 +0100

    Removed decorative CheckoutBtn component in favor of simple button with location's pathname check

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,16 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
-import CheckoutBtn from "../CheckoutBtn/CheckoutBtn";
 
 import './NavigationBar.module.css';
 
 function NavigationBar({ shoppingCart }) {
+  const location = useLocation()
+
   return (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/shop">Shop</Link>
       <span>Your cart currently has {shoppingCart.length} items. </span>
-      <CheckoutBtn></CheckoutBtn>
+      { (location.pathname == "/shop") ? <button className="checkOutBtn">Checkout</button> : "" }
     </nav>
   )
 }
