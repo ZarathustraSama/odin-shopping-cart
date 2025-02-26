86ec6ce97ef239f1a5066d0ccacacbaaf79effc4
ZarathustraSama <mr.gabriel@live.it>
2025-02-26 18:48:29 +0100

    Removed useLocation hook, added default prop

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,17 +1,15 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import './NavigationBar.module.css';
 
-function NavigationBar({ shoppingCart }) {
-  const location = useLocation()
+function NavigationBar({ shoppingCart = [] }) {
 
   return (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/shop">Shop</Link>
-      <span>Your cart currently has {shoppingCart.length} items. </span>
-      { (location.pathname == "/shop") ? <button className="checkOutBtn">Checkout</button> : "" }
+      <span>Your cart currently has {shoppingCart.length} items.</span>
     </nav>
   )
 }
