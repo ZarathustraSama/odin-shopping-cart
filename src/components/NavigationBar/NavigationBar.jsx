1e827615c92d5d769051189fd2764ca5105d5566
ZarathustraSama <mr.gabriel@live.it>
2025-02-27 18:51:53 +0100

    Added function to count items in cart, refactored html accordingly

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -3,19 +3,34 @@ import PropTypes from "prop-types";
 
 import './NavigationBar.module.css';
 
-function NavigationBar({ shoppingCart = [] }) {
+function NavigationBar({ shoppingCart = [], checkout = false }) {
+
+  function countItems(shoppingItems) {
+    let count = 0;
+
+    console.log(shoppingItems)
+    for (let i = 0; i < shoppingItems.length; i++) {
+      console.log("Here")
+      count += shoppingItems[i].quantity;
+      
+    }
+    console.log(count)
+    return count;
+  }
 
   return (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/shop">Shop</Link>
-      <span>Your cart currently has {shoppingCart.length} items.</span>
+      <span>Your cart currently has {countItems(shoppingCart)} items. </span>
+      {checkout ? <button className="checkoutBtn">Checkout</button> : ""}
     </nav>
   )
 }
 
 NavigationBar.propTypes = {
   shoppingCart: PropTypes.array.isRequired,
+  checkout: PropTypes.bool.isRequired,
 }
 
 export default NavigationBar;
