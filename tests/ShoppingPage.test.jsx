e498b18638f2c846fc4fb7a66efe26dafdbe7d63
ZarathustraSama <mr.gabriel@live.it>
2025-02-27 22:32:03 +0100

    Added tests for rendering/fetching elements

--- tests/ShoppingPage.test.jsx
+++ tests/ShoppingPage.test.jsx
@@ -1,12 +1,20 @@
 import { describe, it, expect } from 'vitest';
-import { render } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import { BrowserRouter } from 'react-router-dom';
 
 import ShoppingPage from '../src/components/ShoppingPage/ShoppingPage';
 
+
 describe("ShoppingPage", () => {
-  it("should render a NavBar component, and a list of items fetched, each put in a card", () => {
+  it("should render a NavBar component before fetching", () => {
     const { container } = render(<BrowserRouter><ShoppingPage /></BrowserRouter>);
     expect(container).toMatchSnapshot();
+  })
+
+  it("should render fetched data", async () => {
+    await act(async() => {
+      render(<BrowserRouter><ShoppingPage /></BrowserRouter>)
+    });
+    expect((await screen.findAllByRole("cartItem")).length).toBe(10);
   })
 })
